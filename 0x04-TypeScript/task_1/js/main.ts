b1636778458e4e7f36e0063dc2ff45759c544324
Henok Samson <henoksamson57@gmail.com>
2024-10-11 19:00:01 +0300

    Refactor TypeScript configuration and webpack setup

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,28 @@
+// task_1/js/main.ts
+
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [propName: string]: any;
+}
+
+const teacher3: Teacher = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false,
+  };
+  
+  console.log(teacher3);
+  
+  // should print
+  // Object
+  // contract: false
+  // firstName: "John"
+  // fullTimeEmployee: false
+  // lastName: "Doe"
+  // location: "London"
