3791edd029b107fbe2b14c544f5c7eec8e0b60d8
Henok Samson <henoksamson57@gmail.com>
2024-10-11 19:14:58 +0300

    Refactor TypeScript code and add StudentClass

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -54,3 +54,18 @@ console.log(director1);
 function printTeacher(firstName: string, lastName: string): string {
     return `${firstName.charAt(0)}. ${lastName}`;
 }
+
+class StudentClass {
+    firstName: string;
+    lastName: string;
+    constructor(firstName: string, lastName: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    workOnHomework() {
+        return 'Currently working';
+    }
+    displayName() {
+        return this.firstName;
+    }
+}
